32201f8e3e2bc98181aaf8fea12bd9975e4e4d81
najihun <jihun.na@hashicorp.com>
2023-10-18 23:36:37 +0900

    commit

--- main.tf
+++ main.tf
@@ -21,11 +21,6 @@ resource "aws_s3_bucket" "lambda_bucket" {
   bucket = random_pet.lambda_bucket_name.id
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  acl    = "private"
-}
-
 data "archive_file" "lambda_hello_world" {
   type = "zip"
 
