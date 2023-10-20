35bfc65d1d0fe5dcc1dadc265200ebb40c452b05
najihun <jihun.na@hashicorp.com>
2023-10-20 13:57:33 +0900

    s3. extension

--- main.tf
+++ main.tf
@@ -49,8 +49,6 @@ resource "aws_lambda_function" "hello_world" {
   source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
 
   role = aws_iam_role.lambda_exec.arn
-  
-  layers = [ "arn:aws:lambda:ap-northeast-2:738900069198:layer:AWS-Parameters-and-Secrets-Lambda-Extension:11" ]
 
   environment {
     variables = {
@@ -59,6 +57,8 @@ resource "aws_lambda_function" "hello_world" {
       DB_PASSWORD = "abcde!@"
     }
   }
+
+  layers = [ "arn:aws:lambda:ap-northeast-2:738900069198:layer:AWS-Parameters-and-Secrets-Lambda-Extension:11" ]
 }
 
 resource "aws_cloudwatch_log_group" "hello_world" {
